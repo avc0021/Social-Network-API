d0f118dc0f933e3db6a4427700b5f48b3f172414
avc0021 <avc0021@gmail.com>
2022-03-04 22:33:58 -0600

    thought model started

---
+++ models/Thought.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+   {
+      thoughtText: {
+        type: String,
+        unique: true,
+        required: 'Please enter text.',
+        maxlength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      username: {
+          type: String,
+          required: true
+      },
+      reactions: [reactionSchema]
+   }
+);
+
+// UserSchema.virtual('friendCount').get(function() {
+//     return this.friends.length;
+// });
--- models/User.js
+++ models/User.js
@@ -1,14 +1,34 @@
-//username
-//  String
-//  Unique
-//  Required
-//  Trimmed
-//email
-//  String
-//  Required
-//  Unique
-//  Must match a valid email address (look into Mongoose's matching validation)
-//thoughts
-//  Array of _id values referencing the Thought model
-//friends
-//  Array of _id values referencing the User model (self-reference)
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+   {
+      username: {
+        type: String,
+        unique: true,
+        required: 'Please enter username.',
+        trime: true
+      },
+      email: {
+        type: String,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+        unique: true,
+
+      },
+      thoughts: [
+          {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought'
+          }
+      ],
+      friends: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+          }
+      ]
+   }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
