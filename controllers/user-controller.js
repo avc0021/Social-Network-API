edb76e2d877336d4c4314a5ccbecd98f877a8cc4
avc0021 <avc0021@gmail.com>
2022-03-06 14:40:49 -0600

    added user controller to add method to get user by ID

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,22 @@ const userController = {
         User.find({}).then(users => {
             res.json(users);
           });
-    }
+    },
+
+    //get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: 'Thought',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(404);
+            });
+    },       
 }
 
 module.exports + userController;
