b5849747e89654dfe289ed2d73dea17bd0c4822d
avc0021 <avc0021@gmail.com>
2022-03-06 17:46:15 -0600

    though controller updated with create method

---
+++ controllers/thought-controller.js
@@ -0,0 +1,29 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    //add thought to user
+    addThought({ params, body }, res) {
+        console.log(params);
+        Thought.create(body)
+            .then(({ _id }) => {
+              return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thought: _id } },
+                { new: true }
+              );
+            })
+            .then (dbUserData => {
+              console.log(dbUserData);
+              if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+              }
+              res.json(dbUserData)
+            })
+            .catch(err => res.json(err));
+    },
+    // add reaction
+
+
+
+}
