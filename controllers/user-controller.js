894db2818265db4bd23dc0ba0e8e6cf3673dd92d
avc0021 <avc0021@gmail.com>
2022-03-07 16:31:20 -0600

    updated path in find user by Id, this now works

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -20,7 +20,7 @@ const userController = {
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
             .populate({
-                path: 'Thought',
+                path: 'thoughts',
                 select: '-__v'
             })
             .select('-__v')
